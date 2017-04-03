983886d724606b381a1d3ef567a9ef03a56f88b0
Vinh Q. DANG <dqvinh87@gmail.com>
2017-04-03 11:51:14 +0200

    change code to simple ensemble

--- indeed_ml/job_tags.R
+++ indeed_ml/job_tags.R
@@ -26,140 +26,145 @@ for (i in 1:12) {
   nfolds <- 5
   
   # GBM Hyperparamters
-  learn_rate_opt <- c(0.01, 0.03)
-  max_depth_opt <- c(3, 4, 5, 6, 9)
-  sample_rate_opt <- c(0.7, 0.8, 0.9, 1.0)
-  col_sample_rate_opt <- c(0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)
-  hyper_params <- list(learn_rate = learn_rate_opt,
-                       max_depth = max_depth_opt,
-                       sample_rate = sample_rate_opt,
-                       col_sample_rate = col_sample_rate_opt)
-  
-  # search_criteria <- list(strategy = "RandomDiscrete",
-  #                         max_models = 3,
-  #                         seed = 1)
-  
-  search_criteria <- list(strategy = "RandomDiscrete", 
-                          stopping_metric = "AUC", 
-                          stopping_tolerance = 0.001, 
-                          stopping_rounds = 10)
-  
-  gbm_grid <- h2o.grid(algorithm = "gbm",
-                       grid_id = "gbm_grid_binomial",
-                       x = x,
-                       y = y,
-                       training_frame = h_train,
-                       ntrees = 100,
-                       seed = 1,
-                       nfolds = nfolds,
-                       fold_assignment = "Modulo",
-                       hyper_params = hyper_params,
-                       search_criteria = search_criteria)
-  
-  # random forest model
-  my_rf <- h2o.randomForest(x = x,
-                            y = y,
-                            training_frame = h_train,
-                            ntrees = 500,
-                            nfolds = nfolds,
-                            fold_assignment = "Modulo",
-                            seed = 1)
-  
-  # deep learning model
-  hyper_params_dl <- list(
-    hidden=list(c(64,64,64),c(128,128),c(512)),
-    input_dropout_ratio=c(0,0.05),
-    rate=c(0.01,0.02),
-    rate_annealing=c(1e-8,1e-7,1e-6)
-  )
-  
-  dl_grid <- h2o.grid(
-    algorithm="deeplearning",
-    grid_id="dl_grid", 
-    training_frame=h_train,
-    nfolds=nfolds, 
-    x=x, 
-    y=y,
-    epochs=20,
-    stopping_metric="AUC",
-    stopping_tolerance=1e-2,        ## stop when misclassification does not improve by >=1% for 2 scoring events
-    stopping_rounds=2,
-    score_validation_samples=10000, ## downsample validation set for faster scoring
-    score_duty_cycle=0.025,         ## don't score more than 2.5% of the wall time
-    adaptive_rate=F,                ## manually tuned learning rate
-    momentum_start=0.5,             ## manually tuned momentum
-    momentum_stable=0.9, 
-    momentum_ramp=1e7, 
-    l1=1e-5,
-    l2=1e-5,
-    activation=c("Rectifier"),
-    max_w2=10,                      ## can help improve stability for Rectifier
-    hyper_params=hyper_params
-  )
-  
-  ensemble <- h2o.stackedEnsemble(x = x,
-                                  y = y,
-                                  training_frame = h_train,
-                                  model_id = "ensemble_gbm_grid_binomial",
-                                  base_models = list(gbm_grid@model_ids,my_rf@model_id, dl_grid$model_ids))
-  
-  perf <- h2o.performance(ensemble, newdata = test)
-  
-  
-  
-  pred <- h2o.predict(ensemble, newdata = test)
-  
-  final_predicts <- cbind (final_predicts, as.vector(pred$predict))
-  
-  # lr <- h2o.glm(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
-  #               family = "binomial")
+  # learn_rate_opt <- c(0.01, 0.03)
+  # max_depth_opt <- c(3, 4, 5, 6, 9)
+  # sample_rate_opt <- c(0.7, 0.8, 0.9, 1.0)
+  # col_sample_rate_opt <- c(0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)
+  # hyper_params <- list(learn_rate = learn_rate_opt,
+  #                      max_depth = max_depth_opt,
+  #                      sample_rate = sample_rate_opt,
+  #                      col_sample_rate = col_sample_rate_opt)
   # 
-  # pre_lr <- h2o.predict(lr, newdata = h_test)
+  # # search_criteria <- list(strategy = "RandomDiscrete",
+  # #                         max_models = 3,
+  # #                         seed = 1)
+  # 
+  # search_criteria <- list(strategy = "RandomDiscrete", 
+  #                         stopping_metric = "AUC", 
+  #                         stopping_tolerance = 0.001, 
+  #                         stopping_rounds = 10)
+  # 
+  # gbm_grid <- h2o.grid(algorithm = "gbm",
+  #                      grid_id = "gbm_grid_binomial",
+  #                      x = x,
+  #                      y = y,
+  #                      training_frame = h_train,
+  #                      ntrees = 100,
+  #                      seed = 1,
+  #                      nfolds = nfolds,
+  #                      fold_assignment = "Modulo",
+  #                      hyper_params = hyper_params,
+  #                      search_criteria = search_criteria)
+  # 
+  # # random forest model
+  # my_rf <- h2o.randomForest(x = x,
+  #                           y = y,
+  #                           training_frame = h_train,
+  #                           ntrees = 500,
+  #                           nfolds = nfolds,
+  #                           fold_assignment = "Modulo",
+  #                           seed = 1)
+  # 
+  # # deep learning model
+  # hyper_params_dl <- list(
+  #   hidden=list(c(64,64,64),c(128,128),c(512)),
+  #   input_dropout_ratio=c(0,0.05),
+  #   rate=c(0.01,0.02),
+  #   rate_annealing=c(1e-8,1e-7,1e-6)
+  # )
+  # 
+  # dl_grid <- h2o.grid(
+  #   algorithm="deeplearning",
+  #   grid_id="dl_grid", 
+  #   training_frame=h_train,
+  #   nfolds=nfolds, 
+  #   x=x, 
+  #   y=y,
+  #   epochs=20,
+  #   stopping_metric="AUC",
+  #   stopping_tolerance=1e-2,        ## stop when misclassification does not improve by >=1% for 2 scoring events
+  #   stopping_rounds=2,
+  #   score_validation_samples=10000, ## downsample validation set for faster scoring
+  #   score_duty_cycle=0.025,         ## don't score more than 2.5% of the wall time
+  #   adaptive_rate=F,                ## manually tuned learning rate
+  #   momentum_start=0.5,             ## manually tuned momentum
+  #   momentum_stable=0.9, 
+  #   momentum_ramp=1e7, 
+  #   l1=1e-5,
+  #   l2=1e-5,
+  #   activation=c("Rectifier"),
+  #   max_w2=10,                      ## can help improve stability for Rectifier
+  #   hyper_params=hyper_params
+  # )
+  # 
+  # ensemble <- h2o.stackedEnsemble(x = x,
+  #                                 y = y,
+  #                                 training_frame = h_train,
+  #                                 model_id = "ensemble_gbm_grid_binomial",
+  #                                 base_models = list(gbm_grid@model_ids,my_rf@model_id, dl_grid$model_ids))
+  # 
+  # perf <- h2o.performance(ensemble, newdata = test)
+  # 
+  # 
+  # 
+  # pred <- h2o.predict(ensemble, newdata = test)
+  # 
+  # final_predicts <- cbind (final_predicts, as.vector(pred$predict))
   
+  lr <- h2o.glm(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                 family = "binomial")
   
-  # gbm1 <- h2o.gbm(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
-  #                 ntrees = 500)
-  # 
-  # pre_gbm1 <- h2o.predict(gbm1, newdata = h_test)
-  # 
-  # rf1 <- h2o.randomForest(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
-  #                         ntrees = 500)
-  # 
-  # pre_rf1 <- h2o.predict(rf1, newdata = h_test)
-  # 
-  # d1 <- h2o.deeplearning(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
-  #                        hidden = c(512))
-  # 
-  # pre_d1 <- h2o.predict(d1, newdata = h_test)
-  # 
-  # d2 <- h2o.deeplearning(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
-  #                        hidden = c(128,128))
-  # 
-  # pre_d2 <- h2o.predict(d2, newdata = h_test)
-  # 
-  # d3 <- h2o.deeplearning(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
-  #                        hidden = c(64,64,64))
-  # 
-  # pre_d3 <- h2o.predict(d3, newdata = h_test)
-  # 
-  # all_pre = cbind (as.vector(pre_lr$predict),
-  #                  as.vector(pre_gbm1$predict),
-  #                  as.vector(pre_rf1$predict),
-  #                  as.vector(pre_d1$predict),
-  #                  as.vector(pre_d2$predict),
-  #                  as.vector(pre_d3$predict))
-  # 
-  # all_pre <- data.frame(all_pre)
-  # 
-  # preds <- c()
-  # 
-  # for (j in 1:nrow(all_pre)) {
-  #   a <- all_pre[j,]
-  #   res = (sum(a=="True") >= 2)
-  #   preds <- c(preds, res)
-  # }
-  # 
-  # final_predicts <- cbind (final_predicts, preds)
+  pre_lr <- h2o.predict(lr, newdata = h_test)
+
+
+  gbm1 <- h2o.gbm(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                 ntrees = 500)
+  pre_gbm1 <- h2o.predict(gbm1, newdata = h_test)
+
+  gbm2 <- h2o.gbm(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                   ntrees = 200, max_depth = 10, learn_rate = 0.01)
+  
+   pre_gbm2 <- h2o.predict(gbm2, newdata = h_test)
+
+  rf1 <- h2o.randomForest(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                           ntrees = 500)
+  
+  pre_rf1 <- h2o.predict(rf1, newdata = h_test)
+  
+  d1 <- h2o.deeplearning(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                          hidden = c(512))
+  
+  pre_d1 <- h2o.predict(d1, newdata = h_test)
+  
+  d2 <- h2o.deeplearning(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                          hidden = c(128,128))
+  
+  pre_d2 <- h2o.predict(d2, newdata = h_test)
+  
+  d3 <- h2o.deeplearning(x=1:(ncol(new_df)-1),y=ncol(new_df),training_frame =  h_train,
+                          hidden = c(64,64,64))
+  
+  pre_d3 <- h2o.predict(d3, newdata = h_test)
+  
+  all_pre = cbind (as.vector(pre_lr$predict),
+                    as.vector(pre_gbm1$predict),
+                    as.vector(pre_gbm2$predict),
+                    as.vector(pre_rf1$predict),
+                    as.vector(pre_d1$predict),
+                    as.vector(pre_d2$predict),
+                    as.vector(pre_d3$predict))
+  
+   all_pre <- data.frame(all_pre)
+  
+   preds <- c()
+  
+  for (j in 1:nrow(all_pre)) {
+     a <- all_pre[j,]
+     res = (sum(a=="True") >= 3)
+     preds <- c(preds, res)
+  }
+  
+  final_predicts <- cbind (final_predicts, preds)
 }
 
 write.table(x=final_predicts, file = "final_predicts.txt", row.names = FALSE, col.names = FALSE)
